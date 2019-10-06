6d0e3703bf3b369cf40983eb70f0cd1220f667c2
Benjamin Gomez <benjamingomez@Benjamins-MacBook-Pro.local>
2019-10-06 14:46:54 -0700

    got wireframe layout working big dipper func working

---
+++ js/app.js
@@ -0,0 +1,68 @@
+console.log('coming in hot!');
+function makeBigDipper(){
+const $div = $('<div/>');
+
+$div.attr('class','starC');
+$div.css('width', '7px');
+$div.css('height', '7px');
+$div.css('top', '477px');
+$div.css('left', '585px');
+
+const $div2 = $('<div/>');
+
+$div2.attr('class','starC');
+$div2.css('width', '9px');
+$div2.css('height', '9px');
+$div2.css('top', '461px');
+$div2.css('left', '714px');
+
+const $div3 =$('<div/>');
+
+$div3.attr('class','starC');
+$div3.css('width', '10px');
+$div3.css('height', '10px');
+$div3.css('top', '338px');
+$div3.css('left', '550px');
+
+const $div4 =$('<div/>');
+
+$div4.attr('class','starC');
+$div4.css('width', '12px');
+$div4.css('height', '12px');
+$div4.css('top', '331px');
+$div4.css('left', '720px');
+
+const $div5 =$('<div/>');
+
+$div5.attr('class','starC');
+$div5.css('width', '9px');
+$div5.css('height', '9px');
+$div5.css('top', '250px');
+$div5.css('left', '850px');
+
+const $div6 =$('<div/>');
+
+$div6.attr('class','starC');
+$div6.css('width', '9px');
+$div6.css('height', '9px');
+$div6.css('top', '200px');
+$div6.css('left', '953px');
+
+const $div7 =$('<div/>');
+
+$div7.attr('class','starC');
+$div7.css('width', '10px');
+$div7.css('height', '10px');
+$div7.css('top', '200px');
+$div7.css('left', '1100px');
+
+$('body').append($div);
+$('body').append($div2);
+$('body').append($div3);
+$('body').append($div4);
+$('body').append($div5);
+$('body').append($div6);
+$('body').append($div7);
+
+}
+makeBigDipper();
